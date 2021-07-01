73b00c6ba1f745a22fab214f1861951becddaf85
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-07-01 14:25:59 -0400

    added pause functionality

--- scripts.js
+++ scripts.js
@@ -3,5 +3,11 @@ console.log(button);
 const audio=document.getElementById(`audio`);
 console.log(audio);
 const playMusicLoop=()=>{audio.play();
-audio.loop=true;}
+audio.loop=true;
+button.removeEventListener("click", playMusicLoop);
+button.addEventListener("click",function(){audio.pause();
+
+    button.addEventListener('click', playMusicLoop);
+})
+}
 button.addEventListener('click', playMusicLoop);
